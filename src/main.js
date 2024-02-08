0b8d53cc20f0e0d73f11db3c65881d80f229df66
Roland Garza <garza.roland24@gmail.com>
2024-02-08 14:55:37 -0500

    Staging Routers

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.min.css' // Import Bootstrap CSS
 
 createApp(App).mount('#app')
