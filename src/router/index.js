1ec12bad53fa9817ec796815dba64185f59397ad
Roland Garza <garza.roland24@gmail.com>
2024-02-12 15:32:33 -0500

    Pushing Routers

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,10 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 //Imported components
 import HomeView from '../views/HomeView.vue'; // Adjust the path if necessary
+import BiographyView from '../views/BiographyView.vue';
+import ContactView from '../views/ContactView.vue';
+import MinigamesView from '../views/MinigamesView';
+import MusicView from '../views/MusicView.vue';
 
 const routes = [
   {
@@ -9,6 +13,26 @@ const routes = [
     name: 'Home',
     component: HomeView,
   },
+  {
+    path: '/biography',
+    name: 'Biography',
+    component: BiographyView,
+  },
+  {
+    path: '/contact',
+    name: 'Contact',
+    component: ContactView,
+  },
+  {
+    path: '/minigames',
+    name: 'Minigames',
+    component: MinigamesView,
+  },
+  {
+    path: '/music',
+    name: 'Music',
+    component: MusicView,
+  }
   // Define other routes here
 ];
 
