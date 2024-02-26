8cefecd444780b02ed296ad373cbc0a6526615a7
Roland Garza <garza.roland24@gmail.com>
2024-02-26 14:59:26 -0500

    Pushing Panel Images

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,12 +6,22 @@
     </video>
   </div>
   <hr>
-  <div class="timeline">
+  <body>
     <div class="container">
-      <div class ></div>
+      <div class="card">
+        <img src="../assets/TheWho_image.png">
+      </div>
+      <div class="card">
+        <img src="../assets/TheWhat_image.png">
+      </div>
+      <div class="card">
+        <img src="../assets/TheWhere_image.png">
+      </div>
+      <div class="card">
+        <img src="../assets/TheWhy_image.png">
+      </div>
     </div>
-
-  </div>
+  </body>
 </template>
 
 <script>
@@ -22,8 +32,55 @@
   max-width: 1080px;
   margin: auto;
 }
+/* Adjust the body to ensure the content is centered */
+
 body {
-  background-color: antiquewhite; /* Sets a light background color for light mode */
+  background-color: antiquewhite;
+}
+.card-container {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #051215;
+}
+
+.container {
+  width: 80%;
+  max-width: 600px;
+  height: 800px;
+  display: flex;
+  justify-content: center;
+  align-items: stretch;
+  gap: 0.50rem;
+  transition: all 400ms;
+}
+
+.card {
+  flex: 1;
+  height: 100%;
+  transition: all 400ms;
+  cursor: pointer;
+}
+.card > img {
+  display: block;
+  max-width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.card:nth-child(odd) {
+  translate: 0 -20px;
+}
+.card:nth-child(even) {
+  translate: 0 20px;
+}
+
+.container:hover .card:not(:hover) {
+  filter: grayscale(100%);
+}
+.card:hover {
+  flex: 3;
 }
 /*
 @media (prefers-color-scheme: dark) {
